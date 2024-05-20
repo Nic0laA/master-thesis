2dc39c5cdff5f17d697ac9e908736368778f90b6
Nic0laA <nic.asquith@gmail.com>
2024-05-20 15:37:45 +0200

    Add tblogs from scratch

--- final_peregrine_runs/results/tb_logs/copy_from_scratch.sh
+++ final_peregrine_runs/results/tb_logs/copy_from_scratch.sh
@@ -11,6 +11,30 @@ for run in AttentionUNet UNet_original UNet_pruned0 UNet_pruned10 UNet_pruned5 V
     # Copy bounds
     cp -f ${scratch_dir}/${run}/bounds*     ${run}
 
+    # Loop over rounds and log versions
+    for round in {1..8}; do
+        for version in 0 1; do
+            
+            folder=${scratch_dir}/${run}/trainer_${run}_R${round}/${run}_R${round}/version_${version}
+            if [ ! -d ${folder} ]; then
+                continue
+            fi
+            if [ ! -d ${run}/R${round} ]; then
+                mkdir -p ${run}/R${round}
+            fi
+
+
+            for file in `ls ${folder}/events*`; do
+
+                name=`basename -a ${file}`
+                cp ${file}  ${run}/R${round}/R${round}_v${version}_${name}
+
+
+
+            done
+
+        done
+    done
 
 
 
